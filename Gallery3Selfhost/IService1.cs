8928beb221f2ae07645f3620e067cc137103957d
L8rlTinkerer <nickumcb@gmail.com>
2016-05-08 16:49:15 +1200

    Added a DAL and created and launhed a WCF service

---
+++ Gallery3Selfhost/IService1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery3Selfhost
+{
+    //  attribute necessary for WCF services
+    [ServiceContract] 
+    interface IService1
+    {
+        //  attribute necessary for WCF services
+        [OperationContract]
+        List<string> GetArtistNames();
+    }
+
+
+    
+}
---
+++ Gallery3Selfhost/Service1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery3Selfhost
+{
+    class Service1:IService1
+    {
+
+
+        // The keyword using allows us to create objects that will be disposed of orderly when no longer needed, even in case of exceptions; an important feature where database connections are concerned: 
+        public List<string> GetArtistNames()
+        { // service function that retrieves a list of all artist names
+            using (Gallery_DataEntities lcContext = new Gallery_DataEntities())
+                return lcContext.Artists.Select(lcArtist => lcArtist.Name).ToList();
+        }
+
+    }
+}
